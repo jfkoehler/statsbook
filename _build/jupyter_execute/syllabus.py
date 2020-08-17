0b138ad3d4cd4eaafc9a0db7448cec21e949b7d1
jfkoehler <koehlerj@newschool.edu>
2020-08-16 20:58:48 -0400

    second try

---
+++ _build/jupyter_execute/syllabus.py
@@ -0,0 +1,123 @@
+# Introduction to Statistics
+
+**LMTH 2030B**: Statistics with SPSS
+
+*Jacob Koehler*: koehlerj@newschool.edu
+
+*Office Hours*: By Appointment
+
+## Course Description
+
+This course is an introduction to statistics using the software package SPSS. Emphasis is on exploring quantitative data and applying concepts to a range of situations. Topics include descriptive statistics, basic probability, normal distributions, correlation, linear regression, and hypothesis tests. The course combines lectures, discussions, and computer assignments. Several class sessions are held at a computer lab to learn specific software skills. Students are expected to go to the lab on a regular basis to complete homework assignments and explore the functionality of SPSS. This course is offered every semester and fulfills the second math requirement for the Interdisciplinary Science major and is a Requirement for the Environmental Studies and Psychology Majors.
+
+## Learning Outcomes
+
+By successful completion of this course, students will be able to:
+
+1. Select methods for collecting and/or analyzing data for statistical inference.
+2. Describe patterns, trends, associations, and relationships in data
+3. Explore random phenomena.
+4. Develop an explanation or justify a conclusion using evidence from data, definitions, or statistical inference.
+
+## Final Grade
+
+1. Weekly Homework Sets 20%
+2. Weekly Group Lab 20%
+3. Final Project 20%
+4. Weekly Quiz 20%
+5. Participation/Attendance 20%
+
+## Materials 
+
+- *Textbook*: Moore et al., *Introduction to the Practice of Statistics: 9th Edition*
+
+- *Technology*: A working laptop that you have administrative rights on to install programs and manipulate files.  We will use the following for software:
+
+- *Google Sheets*: Available through your New School Email 
+- *IBM SPSS*: Available through [Apps Anywhere](https://it.newschool.edu/services/learning-resources/apps-anywhere)
+- *Jupyter Notebooks*: Freely downloadable through [Anaconda](https://www.anaconda.com/products/individual). Please see the following videos for assistance downloading and installing: 
+ - [**Windows**](https://www.youtube.com/watch?v=xxQ0mzZ8UvA)
+ - [**Mac**](https://www.youtube.com/watch?v=TcSAln46u9U)
+  
+
+
+### Course Schedule:
+
+
+| Week/Class/Session | Date/ Date Range | Topic or Activity | Assignment |
+| ----- | ----- | ----- | ------ |
+| w1d1 | 8.31.20 | Introduction to class, Loading and accessing data, data types | Setup |
+| w1d2 | 9.2.20 | Describing Data, Center and Spread | Homework 1 |
+| w2d1 | 9.7.20 | no class | na |
+| w2d2 | 9.9.20 | Data Visualization I | Homework 2 |
+| w3d3 | 9.14.20 | Normal Distributions | na |
+| w3d2 | 9.16.20 | tbd | tbd |
+| w4d1 | 9.21.20 | Bivariate Data: Categorical, Quantitative | na |
+| w4d2 | 9.23.20 | Correlation | Homework 3 |
+| w5d1 | 9.28.20 | No Class | No Class |
+| w5d2 | 9.30.20 | Linear Regression | na |
+| w6d1 | 10.5.20 | non-Linear Models | Homework 4 |
+| w6d2 | 10.7.20 | Regression Lab | na |
+| w7d1 | 10.12.20 | Experimental Design | na |
+| w7d2 | 10.14.20 | Sampling | Homework 5 |
+| w8d1 | 10.19.20 | Inference and Experiments | na | 
+| w8d2 | 10.21.20 | Collecting Data lab | Homework 6 |
+| w9d1 | 10.26.20 | Introduction to Probability | na |
+| w9d2 | 10.28.20 | Conditional Probability | Homework 7 |
+| w10d1 | 11.2.20 | Binomial Distributions | na |
+| w10d2 | 11.4.20 | Geometric and Poisson Distributions | Homework 8 |
+| w11d1 | 11.9.20 | Central Limit Theorem, distributions for sample proportions and differences in sample proportions | na |
+| w11d2 | 11.11.20 | Confidence Intervals and Means | Homework 9 |
+| w12d1 | 11.16.20 | Chi-Square Goodness of Fit | na |
+| w12d2 | 11.18.20 | Confidence Interval and Regression | Homework 10 |
+| w13d1 | 11.23.20 | Regression and Inference | na |
+|w13d2 | 11.25.20 | no class | na |
+| w14d1 | 11.30.20 | Logistic Regression and Classification | na |
+| w14d2 | 12.2.20 | API Workshop | Homework 11 |
+| w15d1 | 12.7.20 | Final Project Workshop I | na |
+| w15d2 | 12.9.20 | Final Project Workshop II | na | 
+
+
+### Policies
+
+*Academic Integrity*
+
+Compromising your academic integrity may lead to serious consequences, including (but not limited to) one or more of the following: failure of the assignment, failure of the course, academic warning, disciplinary probation, suspension from the university, or dismissal from the university. 
+
+Students are responsible for understanding the University’s policy on academic honesty and integrity and must make use of proper citations of sources for writing papers, creating, presenting, and performing their work, taking examinations, and doing research. It is the responsibility of students to learn the procedures specific to their discipline for correctly and appropriately differentiating their own work from that of others. The full text of the policy, including adjudication procedures, is found at http://www.newschool.edu/policies/
+
+*Participation/ Attendance/ Expectations for the Remote Learning Environment*
+
+
+Participation is an essential part of class and includes: keeping up with reading, contributing meaningfully to class discussions, active participation in group work, and attending synchronous sessions regularly and on time. Students are expected to keep up with class activities and requirements each week.
+
+
+*Student Course Ratings (Course Evaluations)*
+
+During the last two weeks of the semester, students are asked to provide feedback for each of their courses through an online survey. They cannot view grades until providing feedback or officially declining to do so. Course evaluations are a vital space where students can speak about the learning experience. It is an important process which provides valuable data about the successful delivery and support of a course or topic to both the faculty and administrators. Instructors rely on course rating surveys for feedback on the course and teaching methods, so they can understand what aspects of the class are most successful in teaching students, and what aspects might be improved or changed in future. Without this information, it can be difficult for an instructor to reflect upon and improve teaching methods and course design. In addition, program/department chairs and other administrators review course surveys. Instructions are available online at http://www.newschool.edu/provost/course-evaluations-student-instructions.pdf. 
+
+*Resources*
+
+The university provides many resources to help students achieve academic and artistic excellence. These resources include:
+- University Libraries and Archives: http://library.newschool.edu (See also Reserves)
+- University Learning Center: http://www.newschool.edu/learning-center 
+- Student Disability Services: www.newschool.edu/student-disability-services/  
+
+If you are a student with a disability/disabled student, or believe you might have a disability that requires accommodations, please contact the Student Disability Services (SDS) at studentdisability@newschool.edu, or 212-229-5626, to coordinate all reasonable accommodation requests.
+
+The New School Food Assistance: For more information on food assistance and additional resources, please click on the link below: https://www.newschool.edu/student-support-crisis-management/student-food-pantry-faq/ 
+
+
+Health and Wellness: For information about services and support available to New School students please click on the link below: https://www.newschool.edu/campus-community/health-wellness-support/
+
+
+
+
+
+```{toctree}
+:hidden:
+:titlesonly:
+
+
+syllabus
+```
